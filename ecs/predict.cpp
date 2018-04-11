947670bf94ab4f7f1fb04d3bce31f73cf045e3b0
wtr <396612789@qq.com>
2018-04-11 16:50:33 +0800

    去噪

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -417,7 +417,7 @@ void denoise()
 		//去除异常数据
 		for (int j = 1; j <= train_day; j++)
 		{
-			if (flavors[i].flavor_number_of_day[j] > 10 * mean)
+			if (flavors[i].flavor_number_of_day[j] - mean > 3 * stddev)
 			{
 				flavors[i].flavor_number_of_day[j] = (int)floor(mean);
 			}
