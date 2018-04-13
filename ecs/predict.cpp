5c3994cd4d1e296472b9188a05fb49c6a5f3da73
wtr <396612789@qq.com>
2018-04-13 19:34:18 +0800

    0.058

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -222,7 +222,7 @@ void tackle_train_record(string flavor_name, string time)
 
 void predict()
 {
-	double a = 0.062;
+	double a = 0.058;
 	double *s1, *s2, *s3;
 	for (unsigned int i = 0; i < flavors.size(); i++)
 	{
