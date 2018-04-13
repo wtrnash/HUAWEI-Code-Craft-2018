1cce1296711cd52ba99cd905149fad55c787d4b4
wtr <396612789@qq.com>
2018-04-13 12:05:38 +0800

    二次指数平滑法调参

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -222,7 +222,7 @@ void tackle_train_record(string flavor_name, string time)
 
 void predict()
 {
-	double a = 0.3;
+	double a = 0.1;
 	double *s1, *s2;
 	for (unsigned int i = 0; i < flavors.size(); i++)
 	{
