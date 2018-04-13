4f257409b9e0e0ea4e6bd2d56122c36460b3209f
wtr <396612789@qq.com>
2018-04-13 13:27:51 +0800

    4.5

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -469,7 +469,7 @@ void denoise()
 		//去除异常数据
 		for (int j = 1; j <= train_day; j++)
 		{
-			if (flavors[i].flavor_number_of_day[j] > 9 * mean)
+			if (flavors[i].flavor_number_of_day[j] - mean > 4.5 * stddev)
 			{
 				flavors[i].flavor_number_of_day[j] = (int)floor(mean);
 			}
